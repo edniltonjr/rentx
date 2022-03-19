584c9c0e62bf13fb856ee153c2f94f8a0e25e294
Ednilton Junior <jrjunior_cg@live.com>
2022-03-18 22:25:55 -0300

    Criando useCase para importar um CSV

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { ImporCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+    constructor(private importCategoryUseCase: ImporCategoryUseCase) {}
+
+    handle(request: Request, response: Response): Response {
+        const { file } = request;
+        this.importCategoryUseCase.execute(file);
+        return response.send();
+    }
+}
+
+export { ImportCategoryController };
