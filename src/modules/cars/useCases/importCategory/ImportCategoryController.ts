01380b04321e92393c8cd347e6a8e350a0ba54df
Ednilton Junior <jrjunior_cg@live.com>
2022-03-24 03:00:20 -0300

    - Implementacao de Injecao de dependencia com tsyringe - Alteracao nas entidades com o typeorm - Atualização da documentação

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ImporCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
-    constructor(private importCategoryUseCase: ImporCategoryUseCase) {}
-
-    handle(request: Request, response: Response): Response {
+    async handle(request: Request, response: Response): Promise<Response> {
         const { file } = request;
-        this.importCategoryUseCase.execute(file);
-        return response.send();
+        const importCategoryUseCase = container.resolve(ImporCategoryUseCase);
+        await importCategoryUseCase.execute(file);
+        return response.status(201).send();
     }
 }
 
