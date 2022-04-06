6c94bfb11498c3b81310d291ead9d4ec197bd52f
Ednilton Junior <jrjunior_cg@live.com>
2022-04-06 16:56:01 -0300

    - Corrigindo Status de um carro - Alterando script de inicialização do servidor

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 import { RentalsRepositoryMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryMemory";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
@@ -8,6 +9,7 @@ import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let dayJsDateProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
@@ -15,9 +17,12 @@ describe("Create Rental", () => {
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentalsRepositoryMemory();
         dayJsDateProvider = new DayjsDateProvider();
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+
         createRentalUseCase = new CreateRentalUseCase(
             rentalsRepositoryInMemory,
-            dayJsDateProvider
+            dayJsDateProvider,
+            carsRepositoryInMemory
         );
     });
 
