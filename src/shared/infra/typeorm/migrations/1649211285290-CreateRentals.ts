b37ff0462b505ad646085e4d8ed2e6cd5d67f83e
Ednilton Junior <jrjunior_cg@live.com>
2022-04-06 01:56:05 -0300

    - Criacao de useCase de rentals - Criacao de um provider para uso de datas

--- src/shared/infra/typeorm/migrations/1649211285290-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1649211285290-CreateRentals.ts
@@ -29,6 +29,7 @@ export class CreateRentals1649211285290 implements MigrationInterface {
                     {
                         name: "end_date",
                         type: "timestamp",
+                        isNullable: true,
                     },
                     {
                         name: "expected_return_date",
@@ -37,6 +38,7 @@ export class CreateRentals1649211285290 implements MigrationInterface {
                     {
                         name: "total",
                         type: "numeric",
+                        isNullable: true,
                     },
                     {
                         name: "created_at",
