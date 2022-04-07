0b9480a2df49551846cd96b316212f83732c9d75
Ednilton Junior <jrjunior_cg@live.com>
2022-04-07 01:11:46 -0300

    - Implementando refresh token na aplicação

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "bf60ea46c2632ef8f5a6235807ac381b",
+    expires_in_token: "15m",
+    secret_refresh_token: "eafcda90c3749a5a50d22f2bf9817a3b",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
