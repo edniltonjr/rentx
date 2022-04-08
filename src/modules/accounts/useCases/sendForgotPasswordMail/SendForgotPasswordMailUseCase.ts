acd817793d3a17f298a7f753211d270fef97d9b2
Ednilton Junior <jrjunior_cg@live.com>
2022-04-08 01:11:58 -0300

    - Adicionando template engine para envio de e-mail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidV4 } from "uuid";
 
@@ -5,8 +6,8 @@ import { IDateProvider } from "@shared/container/providers/DateProvider/IDatePro
 import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 
-import { IUsersRepository } from "../repositories/IUsersRepository";
-import { IUsersTokensRepository } from "../repositories/IUsersTokensRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersTokensRepository } from "../../repositories/IUsersTokensRepository";
 
 @injectable()
 class SendForgotPasswordMailUseCase {
@@ -23,6 +24,15 @@ class SendForgotPasswordMailUseCase {
     async execute(email: string) {
         const user = await this.usersRepository.findByEmail(email);
 
+        const templatePath = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "emails",
+            "forgotPassword.hbs"
+        );
+
         if (!user) {
             throw new AppError("User does not exists!");
         }
@@ -36,10 +46,16 @@ class SendForgotPasswordMailUseCase {
             expires_date,
         });
 
+        const variables = {
+            name: user.name,
+            link: `${process.env.FORGOT_MAIL_URL}${token}`,
+        };
+
         await this.mailProvider.sendMail(
             email,
             "Recuperação de senha",
-            `O link para o reset é ${token}`
+            variables,
+            templatePath
         );
     }
 }
