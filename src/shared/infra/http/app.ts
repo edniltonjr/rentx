168f9d2abb3fd993cfc58b2833dba89c0dfc7b59
Ednilton Junior <jrjunior_cg@live.com>
2022-04-10 01:06:01 -0300

    - Adicionando o Sentry ao projeto

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
@@ -19,7 +21,21 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
@@ -27,10 +43,7 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-    return response.json({ name });
-});
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
